3bc90c5b9260e51b13fe85f2e5b9355d656da04b
Priscila Lessa <prillybsb@gmail.com>
2023-09-07 19:49:07 -0300

    atualizando

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 
-fs.writeFile(
+/*fs.writeFile(
   "./files/arquivo.txt",
   "Esse é meu primeiro arquivo",
   function (err) {
@@ -8,4 +8,25 @@ fs.writeFile(
       return console.log(`algo deu errado: ${err} `);
     }
   }
-);
+);*/
+
+/*fs.readFile("./files/arquivo.txt", utf8, function (err, data) {
+  if (err) {
+    return console.log(`algo deu errado: ${err} `);
+  }
+  console.log(data);
+});
+
+fs.rename("./files/arquivo.txt ","./files/novo-nome.txt", function (err) {
+  if (err) {
+    return console.log(`algo deu errado: ${err} `);
+  }
+  console.log("renomeado com sucesso");
+});
+*/
+
+fs.unlink("./files/arquivo.txt", function (err) {
+  if (err) {
+    return console.log(`algo deu errado: ${err} `);
+  }
+});
